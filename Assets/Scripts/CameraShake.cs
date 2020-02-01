1352a72c944391fcdaee15cd9ef7af0bf4f4ca45
Edward Atencio <ed.atencio16@gmail.com>
2020-02-01 11:08:47 -0500

    Camera shake script and zero friction player

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using NaughtyAttributes;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] private float duration = 0.25f;
+    [SerializeField] private float amount = 0.7f;
+    [SerializeField] private float decreaseFactor = 1.0f;
+    [SerializeField] private bool startOnEnable;
+
+    private Vector3 startPosition;
+    private float shakeDuration;
+
+    private void OnEnable()
+    {
+        startPosition = transform.localPosition;
+
+        if (startOnEnable)
+            Run();
+    }
+
+    private void Update()
+    {
+        if (IsRunning)
+        {
+            if (shakeDuration > 0)
+            {
+                transform.localPosition = startPosition + Random.insideUnitSphere * amount;
+                shakeDuration -= Time.deltaTime * decreaseFactor;
+            }
+            else
+            {
+                IsRunning = false;
+                transform.localPosition = startPosition;
+            }
+        }
+    }
+
+    public bool IsRunning { get; private set; }
+
+    [Button]
+    public void Run()
+    {
+        IsRunning = true;
+        shakeDuration = duration;
+    }
+
+    public void Run(float duration)
+    {
+        IsRunning = true;
+        shakeDuration = duration;
+    }
+
+    [Button]
+    public void Stop()
+    {
+        IsRunning = false;
+    }
+}
