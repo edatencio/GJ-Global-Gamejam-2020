b659eb701e16dc7a9000544ec076ec9e0264b56a
Sekiganichi <54489117+Sekiganichi@users.noreply.github.com>
2020-02-01 17:14:24 -0500

    Add files via upload

---
+++ Assets/Scripts/spawn_enemies.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_enemies : MonoBehaviour
+{
+    public GameObject spawnpoint1, spawnpoint2, spawnpoint3;
+    public GameObject robot;
+    private float timer;
+    public float tiempodespawn;
+    public int contador;
+    [SerializeField]private float randomnumber;
+
+    private void Start()
+    {
+        timer = 0;
+        contador = 0;
+        tiempodespawn = 3;
+
+    }
+    private void Update()
+    {
+        if (contador < 15)
+        {
+            timer = timer + Time.deltaTime;
+            if (timer > tiempodespawn)
+            {
+
+                timer = 0;
+                contador = contador + 1;
+                randomnumber = Random.Range(0, 4);
+                if (randomnumber <= 1)
+                {
+                    Instantiate(robot, spawnpoint1.transform.position, transform.rotation);
+                }
+                if (randomnumber > 1 && randomnumber <= 2)
+                {
+                    Instantiate(robot, spawnpoint2.transform.position, transform.rotation);
+                }
+                if (randomnumber > 2 && randomnumber <= 3)
+                {
+                    Instantiate(robot, spawnpoint3.transform.position, transform.rotation);
+                }
+            }
+        }
+        
+    }
+
+
+}
