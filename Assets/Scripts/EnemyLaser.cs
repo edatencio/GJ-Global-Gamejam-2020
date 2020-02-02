1260ea8ae86f7557bbefc7d5bb640b58a7a35e97
Sekiganichi <54489117+Sekiganichi@users.noreply.github.com>
2020-02-02 11:03:52 -0500

    laser de mejor tamaño

--- Assets/Scripts/EnemyLaser.cs
+++ Assets/Scripts/EnemyLaser.cs
@@ -5,6 +5,7 @@ public class EnemyLaser : MonoBehaviour
     public float speed;
     private float timer;
     public GameObject jugador;
+    private float scaleX;
 
     private void Start()
     {
@@ -16,10 +17,20 @@ public class EnemyLaser : MonoBehaviour
     private void Update()
     {
         Vector3 direction = new Vector3(jugador.transform.position.x - transform.position.x, 0f, 0f);
-        transform.Translate(direction * speed * Time.deltaTime);
-
+        transform.Translate((direction.normalized) * speed * Time.deltaTime);
+        if ((jugador.transform.position.x - transform.position.x) < 0)
+        {
+            transform.localScale = transform.localScale.With(x: -scaleX);
+        }
         timer = timer + Time.deltaTime;
         if (timer > 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
