eab412766eda6d3e5cadd01043f4b2ae5aa977a8
nathalie_barbosa <nathalie.barbosa@atomicstudio.com.co>
2020-02-02 15:09:44 -0500

    Menu and loading scene integrated

---
+++ Assets/Scripts/Menu/LoadMainGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadMainGame : MonoBehaviour
+{
+    [SerializeField] public float secondsToWait;
+    [SerializeField] public string sceneName;
+    void Start()
+    {
+        StartCoroutine(_LoadSceneDelayed());
+    }
+
+    public IEnumerator _LoadSceneDelayed()
+    {
+        Debug.Log("Antes de cargar");
+
+        yield return new WaitForSeconds(secondsToWait);
+        Debug.Log("Deberia cargar");
+        SceneManager.LoadScene(sceneName);
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //[SerializeField] public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
